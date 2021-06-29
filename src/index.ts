aecb0e4ee848b24f2723cf950348298c0bd81f35
Gareth Dyson <4315423+dysonic@users.noreply.github.com>
2021-06-29 20:58:13 +1200

    WIP. Initial stab at getting templating working and redis integration.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,48 @@
 import express from 'express';
 import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import redis from 'redis';
 const app = express();
 const port = 3000;
+import { User } from './types/user';
+import { asyncClient } from './helpers/redis';
+import { isLoggedIn } from './retwis';
+import path from 'path';
+
+declare module 'express-session' {
+  interface SessionData {
+    user: User | null;
+  }
+}
+
+const client = redis.createClient();
+const clientAsync = asyncClient(client);
+
+client.on('error', error => {
+  // tslint:disable-next-line:no-console
+  console.error(error);
+});
 
 app.use(logger('dev'));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cookieParser());
+app.use(session({ secret: 'Harry Potter', name: 'auth', saveUninitialized: false, resave: false }))
+app.set('views', path.resolve(__dirname, '../views'));
+app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  const isAuthenticated = await isLoggedIn(req, clientAsync);
+  // tslint:disable-next-line:no-console
+  console.log('isAuthenticated:', isAuthenticated);
+
+  if (!isAuthenticated) {
+    return res.render('index');
+  }
+
   res.send('Hello World!')
+  // res.render('home');
 });
 
 app.listen(port, () => {
---
+++ src/login.ts
@@ -0,0 +1,33 @@
+// import retwis from ./retwis';
+import { Application, Request, Response } from 'express';
+
+export const login = (app: Application) => {
+  app.post('/login', (req: Request, res: Response) => {
+
+    // tslint:disable-next-line:no-console
+    console.log(req.body)
+    res.json(req.body)
+
+    // Form sanity checks
+    /*
+    if (!gt("username") || !gt("password"))
+        goback("You need to enter both username and password to login.");
+
+    //  The form is ok, check if the username is available
+    $username = gt("username");
+    $password = gt("password");
+    $r = redisLink();
+    $userid = $r->hget("users",$username);
+    if (!$userid)
+        goback("Wrong username or password");
+    $realpassword = $r->hget("user:$userid","password");
+    if ($realpassword != $password)
+        goback("Wrong useranme or password");
+
+    # Username / password OK, set the cookie and redirect to index.php
+    $authsecret = $r->hget("user:$userid","auth");
+    setcookie("auth",$authsecret,time()+3600*24*365);
+    header("Location: index.php");
+    */
+  });
+};
